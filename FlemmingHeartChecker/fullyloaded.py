be12fe4c9990c0e0333aae76db2a7d2af801047d
aimwps <mwpskinner@gmail.com>
2021-05-10 08:54:11 +0100

    Completed the CSV/pickle functions

--- FlemmingHeartChecker/fullyloaded.py
+++ FlemmingHeartChecker/fullyloaded.py
@@ -1,6 +1,7 @@
 import os.path
 import pickle
 import datetime as dt
+import pandas as pd
 
 
 #### Create a pipeline for testing different ML models
@@ -16,7 +17,28 @@ import datetime as dt
 ################################################################################
 ### Helper Functions
 
-#def pickle_model(savename, model):
-now_code = dt.datetime.now().strftime("%y%m%d%H%M")
+def no_overwrite_pickle(save_name, model):
+    now_code = dt.datetime.now().strftime("%y%m%d%H%M")
+    new_file_name = save_name + "_" + now_code + ".pickle"
+    duplicate_counter = 1
+    while os.path.exists(new_file_name):
+        new_file_name = save_name + "_"  + now_code + f"_{str(duplicate_counter)}" + ".pickle"
+        duplicate_counter += 1
+    pickle.dump(model, open(new_file_name, 'wb'))
 
-print(now_code)
+def no_overwrite_to_csv(save_name, dataframe):
+    now_code = dt.datetime.now().strftime("%y%m%d%H%M")
+    new_file_name = save_name + "_" + now_code + ".csv"
+    duplicate_counter = 1
+    while os.path.exists(new_file_name):
+        new_file_name = save_name + "_"  + now_code + f"_{str(duplicate_counter)}" + ".csv"
+        duplicate_counter += 1
+    dataframe.to_csv(new_file_name)
+
+
+df = pd.DataFrame({"A":[1,2,3], "B":[4,5,6]})
+
+#test = TestClass("Mark", [1,2,3,4,5,6])
+
+#no_overwrite_pickle("test", test)
+no_overwrite_to_csv("dataframe", df)
